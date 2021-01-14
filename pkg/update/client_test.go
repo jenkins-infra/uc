67062ee138cf8ad65036e62c0e8d61b63b68231c
Gareth Evans <gareth@bryncynfelin.co.uk>
2021-01-14 10:35:22 +0000

    chore: add a flag to display updates to stdout

--- pkg/update/client_test.go
+++ pkg/update/client_test.go
@@ -2,6 +2,7 @@ package update_test
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/garethjevans/uc/pkg/update"
@@ -9,6 +10,104 @@ import (
 	"github.com/garethjevans/uc/pkg/api"
 
 	"github.com/stretchr/testify/assert"
+)
+
+var (
+	versionSet = `ansicolor:0.7.3
+antisamy-markup-formatter:2.1
+authentication-tokens:1.4
+azure-container-agents:1.2.1
+azure-vm-agents:1.5.2
+basic-branch-build-strategies:1.3.2
+blueocean:1.24.3
+blueocean-autofavorite:1.2.4
+blueocean-commons:1.24.3
+blueocean-config:1.24.3
+blueocean-core-js:1.24.3
+blueocean-dashboard:1.24.3
+blueocean-display-url:2.4.0
+blueocean-events:1.24.3
+blueocean-git-pipeline:1.24.3
+blueocean-github-pipeline:1.24.3
+blueocean-i18n:1.24.3
+blueocean-jira:1.24.3
+blueocean-jwt:1.24.3
+blueocean-personalization:1.24.3
+blueocean-pipeline-api-impl:1.24.3
+blueocean-pipeline-editor:1.24.3
+blueocean-pipeline-scm-api:1.24.3
+blueocean-rest:1.24.3
+blueocean-rest-impl:1.24.3
+blueocean-web:1.24.3
+branch-api:2.6.3
+build-name-setter:2.1.0
+cloud-stats:0.26
+config-file-provider:3.7.0
+configuration-as-code:1.46
+credentials:2.3.14
+credentials-binding:1.24
+datadog:2.6.0
+ec2:1.56
+embeddable-build-status:2.0.3
+extended-read-permission:3.2
+git:4.5.2
+git-client:3.6.0
+github:1.32.0
+github-api:1.117
+github-branch-source:2.9.3
+github-checks:1.0.8
+github-label-filter:1.0.0
+groovy:2.3
+inline-pipeline:1.0.1
+javadoc:1.6
+jira:3.1.3
+job-dsl:1.77
+junit:1.48
+kubernetes:1.28.5
+kubernetes-credentials:0.7.0
+kubernetes-credentials-provider:0.15
+ldap:2.2
+lockable-resources:2.10
+matrix-auth:2.6.4
+matrix-project:1.18
+metrics:4.0.2.7
+pipeline-build-step:2.13
+pipeline-github:2.7
+pipeline-graph-analysis:1.10
+pipeline-input-step:2.12
+pipeline-milestone-step:1.3.1
+pipeline-model-api:1.7.2
+pipeline-model-definition:1.7.2
+pipeline-model-extensions:1.7.2
+pipeline-rest-api:2.19
+pipeline-stage-step:2.5
+pipeline-stage-tags-metadata:1.7.2
+pipeline-stage-view:2.19
+pipeline-utility-steps:2.6.1
+plain-credentials:1.7
+prometheus:2.0.8
+scm-api:2.6.4
+scm-filter-branch-pr:0.5.1
+script-security:1.75
+ssh-agent:1.20
+ssh-credentials:1.18.1
+support-core:2.72
+timestamper:1.11.8
+token-macro:2.14
+toolenv:1.2
+variant:1.4
+warnings-ng:8.6.0
+workflow-aggregator:2.6
+workflow-api:2.40
+workflow-basic-steps:2.23
+workflow-cps:2.87
+workflow-cps-global-lib:2.17
+workflow-durable-task-step:2.37
+workflow-job:2.40
+workflow-multibranch:2.22
+workflow-scm-step:2.11
+workflow-step-api:2.23
+workflow-support:3.7`
 )
 
 func TestCanParseUpdateCentre_SingleDependency(t *testing.T) {
@@ -64,7 +163,7 @@ func TestCanParseUpdateCentre_MultipleDependencies(t *testing.T) {
 	assertContainsWithVersion(t, deps, "script-security", "1.54")
 }
 
-func TestCanParseUpdateCentre_CompleteList(t *testing.T) {
+func TestCanParseUpdateCentre_CompleteList_NoVersions(t *testing.T) {
 	u := update.Updater{}
 
 	http := &api.FakeHTTP{}
@@ -178,12 +277,10 @@ func TestCanParseUpdateCentre_CompleteList(t *testing.T) {
 	assertContains(t, deps, "scm-api")
 	assertContains(t, deps, "workflow-support")
 
-	for _, d := range deps {
-		t.Log(d.String())
-	}
+	t.Log(update.AsStrings(deps))
 }
 
-func TestCanParseUpdateCentre_CompleteListWithVersions(t *testing.T) {
+func TestCanParseUpdateCentre_CompleteList_WithVersions(t *testing.T) {
 	u := update.Updater{}
 
 	http := &api.FakeHTTP{}
@@ -192,103 +289,7 @@ func TestCanParseUpdateCentre_CompleteListWithVersions(t *testing.T) {
 
 	http.StubWithFixture(200, "golden.full.json")
 
-	depsIn, err := update.FromStrings([]string{
-		"ansicolor:0.0.1",
-		"antisamy-markup-formatter:2.1",
-		"authentication-tokens:1.4",
-		"azure-container-agents:1.2.1",
-		"azure-vm-agents:1.5.2",
-		"basic-branch-build-strategies:1.3.2",
-		"blueocean:1.24.3",
-		"blueocean-autofavorite:1.2.4",
-		"blueocean-commons:1.24.3",
-		"blueocean-config:1.24.3",
-		"blueocean-core-js:1.24.3",
-		"blueocean-dashboard:1.24.3",
-		"blueocean-display-url:2.4.0",
-		"blueocean-events:1.24.3",
-		"blueocean-git-pipeline:1.24.3",
-		"blueocean-github-pipeline:1.24.3",
-		"blueocean-i18n:1.24.3",
-		"blueocean-jira:1.24.3",
-		"blueocean-jwt:1.24.3",
-		"blueocean-personalization:1.24.3",
-		"blueocean-pipeline-api-impl:1.24.3",
-		"blueocean-pipeline-editor:1.24.3",
-		"blueocean-pipeline-scm-api:1.24.3",
-		"blueocean-rest:1.24.3",
-		"blueocean-rest-impl:1.24.3",
-		"blueocean-web:1.24.3",
-		"branch-api:2.6.3",
-		"build-name-setter:2.1.0",
-		"cloud-stats:0.26",
-		"config-file-provider:3.7.0",
-		"configuration-as-code:1.46",
-		"credentials:2.3.14",
-		"credentials-binding:1.24",
-		"datadog:2.6.0",
-		"ec2:1.56",
-		"embeddable-build-status:2.0.3",
-		"extended-read-permission:3.2",
-		"git:4.5.2",
-		"git-client:3.6.0",
-		"github:1.32.0",
-		"github-api:1.117",
-		"github-branch-source:2.9.3",
-		"github-checks:1.0.8",
-		"github-label-filter:1.0.0",
-		"groovy:2.3",
-		"inline-pipeline:1.0.1",
-		"javadoc:1.6",
-		"jira:3.1.3",
-		"job-dsl:1.77",
-		"junit:1.48",
-		"kubernetes:1.28.5",
-		"kubernetes-credentials:0.7.0",
-		"kubernetes-credentials-provider:0.15",
-		"ldap:2.2",
-		"lockable-resources:2.10",
-		"matrix-auth:2.6.4",
-		"matrix-project:1.18",
-		"metrics:4.0.2.7",
-		"pipeline-build-step:2.13",
-		"pipeline-github:2.7",
-		"pipeline-graph-analysis:1.10",
-		"pipeline-input-step:2.12",
-		"pipeline-milestone-step:1.3.1",
-		"pipeline-model-api:1.7.2",
-		"pipeline-model-definition:1.7.2",
-		"pipeline-model-extensions:1.7.2",
-		"pipeline-rest-api:2.19",
-		"pipeline-stage-step:2.5",
-		"pipeline-stage-tags-metadata:1.7.2",
-		"pipeline-stage-view:2.19",
-		"pipeline-utility-steps:2.6.1",
-		"plain-credentials:1.7",
-		"prometheus:2.0.8",
-		"scm-api:2.6.4",
-		"scm-filter-branch-pr:0.5.1",
-		"script-security:1.75",
-		"ssh-agent:1.20",
-		"ssh-credentials:1.18.1",
-		"support-core:2.72",
-		"timestamper:1.11.8",
-		"token-macro:2.14",
-		"toolenv:1.2",
-		"variant:1.4",
-		"warnings-ng:8.6.0",
-		"workflow-aggregator:2.6",
-		"workflow-api:2.40",
-		"workflow-basic-steps:2.23",
-		"workflow-cps:2.87",
-		"workflow-cps-global-lib:2.17",
-		"workflow-durable-task-step:2.37",
-		"workflow-job:2.40",
-		"workflow-multibranch:2.22",
-		"workflow-scm-step:2.11",
-		"workflow-step-api:2.23",
-		"workflow-support:3.7",
-	})
+	depsIn, err := update.FromStrings(strings.Split(versionSet, "\n"))
 	assert.NoError(t, err)
 
 	assert.NoError(t, err)
@@ -301,9 +302,40 @@ func TestCanParseUpdateCentre_CompleteListWithVersions(t *testing.T) {
 
 	assertContainsWithVersion(t, deps, "ansicolor", "0.7.3")
 
-	for _, d := range deps {
-		t.Log(d.String())
-	}
+	t.Log(update.AsStrings(deps))
+}
+
+func TestCanParseUpdateCentre_CompleteList_CanDetectUpdates(t *testing.T) {
+	u := update.Updater{}
+
+	http := &api.FakeHTTP{}
+	client := api.NewClient(api.ReplaceTripper(http))
+	u.SetClient(client)
+
+	http.StubWithFixture(200, "golden.full.json")
+
+	updatedVersionSet := strings.ReplaceAll(versionSet, "ansicolor:0.7.3", "ansicolor:0.0.1")
+	depsIn, err := update.FromStrings(strings.Split(updatedVersionSet, "\n"))
+	assert.NoError(t, err)
+
+	assert.NoError(t, err)
+	deps, err := u.LatestVersions(depsIn)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 95, len(deps))
+	assertContains(t, deps, "scm-api")
+	assertContains(t, deps, "workflow-support")
+
+	assertContainsWithVersion(t, deps, "ansicolor", "0.7.3")
+
+	t.Log(update.AsStrings(deps))
+
+	changed := update.FindAll(deps, func(info update.DepInfo) bool {
+		return info.Changed
+	})
+
+	t.Log(update.AsStrings(changed))
+	assert.Equal(t, 1, len(changed))
 }
 
 func assertContains(t *testing.T, deps []update.DepInfo, name string) {
