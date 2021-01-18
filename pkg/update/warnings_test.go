240a59abaaa624c491882d890a38adaca83b0c46
Gareth Evans <gareth@bryncynfelin.co.uk>
2021-01-18 09:18:34 +0000

    chore: a test to check if a version match for a warning

---
+++ pkg/update/warnings_test.go
@@ -0,0 +1,24 @@
+package update_test
+
+import (
+	"testing"
+
+	"github.com/garethjevans/uc/pkg/update"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCanEvaluateWarnings(t *testing.T) {
+	w := update.WarningInfo{
+		Name: "google-login",
+		Versions: []update.VersionInfo{
+			{
+				LastVersion: "1.1",
+				Pattern:     `1[.][01](|[.-].*)`,
+			},
+		},
+	}
+
+	assert.True(t, w.Versions[0].Matches("1.0"))
+	assert.True(t, w.Versions[0].Matches("1.1"))
+	assert.False(t, w.Versions[0].Matches("1.2"))
+}
