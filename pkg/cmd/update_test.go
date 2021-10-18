8a4fb64a565f377193e11b20fe359011a9d4a94d
Damien Duportal <damien.duportal@gmail.com>
2021-10-18 16:18:16 +0200

    chore(tests) hotfix on failing tests due to a new antisamy plugin released. Not ideal test data though.
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- pkg/cmd/update_test.go
+++ pkg/cmd/update_test.go
@@ -16,7 +16,7 @@ func ExampleDisplayUpdatesFromPluginTxt() {
 
 	// Output:
 	// ansicolor:1.0.0
-	// antisamy-markup-formatter:2.1
+	// antisamy-markup-formatter:2.3
 	// authentication-tokens:1.4
 }
 
@@ -31,6 +31,6 @@ func ExampleDisplayUpdatesFromValuesYaml() {
 
 	// Output:
 	// ansicolor:1.0.0
-	// antisamy-markup-formatter:2.1
+	// antisamy-markup-formatter:2.3
 	// authentication-tokens:1.4
 }
